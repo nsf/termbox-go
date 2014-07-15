e9227d640138066e099db60f3010bd8d55c8da72
nsf <no.smile.face@gmail.com>
2014-07-15 14:13:50 +0600

    Use github.com/mattn/go-runewidth library.

--- api.go
+++ api.go
@@ -2,6 +2,7 @@
 
 package termbox
 
+import "github.com/mattn/go-runewidth"
 import "fmt"
 import "os"
 import "os/signal"
@@ -157,7 +158,7 @@ func Flush() error {
 			if back.Ch < ' ' {
 				back.Ch = ' '
 			}
-			w := rune_width(back.Ch)
+			w := runewidth.RuneWidth(back.Ch)
 			if *back == *front {
 				x += w
 				continue
